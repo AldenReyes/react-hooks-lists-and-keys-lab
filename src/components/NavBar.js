b9484233e8ee8158708ac87f165655f453911817
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-05-27 08:03:10 -0700

    Completes Lab; transforms a list of values into a list of components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
   const linkElements = links.map((link) => {
-    return <a key={link} href={link}>{link}</a>;
+    return (
+      <a key={link} href={`#` + link}>
+        {link}
+      </a>
+    );
   });
 
   return <nav>{linkElements}</nav>;
